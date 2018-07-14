9e4317d942487143287d273890db7e280e1bd55b
puma0423 <puma0423@gmail.com>
2018-07-14 23:10:08 +0800

    修正訂單總金額BUG

--- src/main/webapp/shoppingCart/js/buyMain.js
+++ src/main/webapp/shoppingCart/js/buyMain.js
@@ -1,12 +1,7 @@
 $(document).ready(function() {
-
-		var allCount = $(".giftContent");
-		var allPrice = $(".giftPrice");
-		var total = 0;
-		for(var i = 0 ; i < allCount.length ; i++){
-			total += ($(allCount[i]).val() * parseInt($(allPrice[i]).text()));
-		}
-		$(".totalPrice").text(total);	
+	totalPrice();	
+	
+	changeCount();
 });
 
 function backMain(){
@@ -15,4 +10,21 @@ function backMain(){
 
 function goToMain2(){
 	window.location.href = '/mvcFoodServer/member9487/goToMain2';	
+}
+function totalPrice(){
+	var allCount = $(".giftContent");
+	var allPrice = $(".giftPrice");
+	var total = 0;
+	for(var i = 0 ; i < allCount.length ; i++){
+		total += ($(allCount[i]).val() * parseInt($(allPrice[i]).text()));
+	}
+	$(".totalPrice").text(total);	
+}
+function changeCount(){
+	var allInput = $(".giftContent");
+	for(var i = 0 ; i < allInput.length ; i++){
+		$(allInput[i]).on("change",function(){			
+			totalPrice();
+		});
+	}
 }
